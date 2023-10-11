6078c446ac9ebbebcd6dde15588e02919605aaf9
Ignacio Curti <ignaciocurtijristo@gmail.com>
2023-10-11 13:22:19 -0300

    primeros endpoints

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-// import express from "express";
-const express = require('express');
-
+import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(3000, () => {
-    console.log("Corriendo en el puerto 3000" );
+const PORT = 8000
+
+app.get('/hola', (req, res) => {
+    res.status(200).send("Esto es un endpoint");
+});
+
+
+app.get('/params/:id', (req, res) => {
+    console.log(`El id enviado por parametro es ${req.params.id}.`)
+    console.log(req)
+    res.status(200).send({"id": req.params.id});
+});
+
+
+app.post('/json', (req, res) => {
+    console.log(req.body.name);
+    res.end();
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Corriendo en el puerto ${PORT}` );
     console.log("Hello Frontend Community");
 });
