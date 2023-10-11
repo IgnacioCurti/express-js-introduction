f121feaf7cd17dc414be5dcb155aea534a8f3de2
Ignacio Curti <ignaciocurtijristo@gmail.com>
2023-10-11 12:36:20 -0300

    initial steps for paso2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello Frontend Community");
+// import express from "express";
+const express = require('express');
+
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Corriendo en el puerto 3000" );
+    console.log("Hello Frontend Community");
+});
